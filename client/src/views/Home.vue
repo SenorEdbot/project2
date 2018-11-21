a81d25255ef163f2acd54b827fb6ff7f0067d99c
Eddie Franco <eddiefranco626@gmail.com>
2018-11-20 21:20:04 -0600

    bootstrapAdded

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,12 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>testing</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'home',
---
+++ client/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-button>I am a Button</b-button>
+    <b-button href="#">I am a Link</b-button> 
+  </div>
+</template>
+
+<script>
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
