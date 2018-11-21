4cb342d410b2176c9edc6236aaf35f4eab50b557
John Caldwell <jmcald0@gmail.com>
2018-11-20 20:42:23 -0600

    ybranch

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Shift Change logo" src="../assets/shift-change.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
