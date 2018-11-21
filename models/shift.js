c5fda00a13b6704df5d0ba8fc264d9f1c70b67de
Eddie Franco <eddiefranco626@gmail.com>
2018-11-20 19:31:57 -0600

    Working connection

--- models/shift.js
+++ models/shift.js
@@ -1,13 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
   const Shifts = sequelize.define('Shifts', {
     name: DataTypes.STRING,
-    reason: DataTypes.TEXT,
+    reason: DataTypes.STRING,
     date: DataTypes.DATE,
     duration: DataTypes.DATE,
     role: DataTypes.STRING,
     covered: DataTypes.BOOLEAN,
     priority: DataTypes.STRING,
-    personResponsible: DataTypes.SRING
+    personResponsible: DataTypes.STRING
   });
   return Shifts;
 };
